71d58d1e3c23c2c507cebd5c6736988dda8e136b
Shotaro Oyama <shotaro12oyama@gmail.com>
2019-01-12 08:43:39 +0900

    Initial Commit

---
+++ udacity-on-the-map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udacity-on-the-map
+//
+//  Created by 尾山昌太郎 on 2019/01/12.
+//  Copyright © 2019年 尾山昌太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
