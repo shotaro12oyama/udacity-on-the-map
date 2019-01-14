d7fbf5b9fab0506b8572236d9dfd2e499c55fb3a
Shotaro Oyama <shotaro12oyama@gmail.com>
2019-01-15 04:40:32 +0900

    login function is still on progress@2019/1/15

---
+++ udacity-on-the-map/Controller/LoginViewController.swift
@@ -0,0 +1,85 @@
+//
+//  LoginViewController.swift
+//  udacity-on-the-map
+//
+//  Created by 尾山昌太郎 on 2019/01/13.
+//  Copyright © 2019年 尾山昌太郎. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var request = URLRequest(url: URL(string: UdacityClient.Endpoints.base)!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        // encoding a JSON body from a string, can also use a Codable struct
+        request.httpBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".data(using: .utf8)
+        print(request.httpBody)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let range = (5..<data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            print(String(data: newData!, encoding: .utf8)!)
+        }
+        task.resume()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginViaWebsiteTapped() {
+    }
+    
+    func handleRequestTokenResponse(success: Bool, error: Error?) {
+    }
+    
+    func handleLoginResponse(success: Bool, error: Error?) {
+    }
+    
+    func handleSessionResponse(success: Bool, error: Error?) {
+        setLoggingIn(false)
+        if success {
+            performSegue(withIdentifier: "completeLogin", sender: nil)
+        } else {
+            showLoginFailure(message: error?.localizedDescription ?? "")
+        }
+    }
+    
+    func setLoggingIn(_ loggingIn: Bool) {
+   }
+    
+    func showLoginFailure(message: String) {
+        let alertVC = UIAlertController(title: "Login Failed", message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        show(alertVC, sender: nil)
+    }
+    
+
+    
+}
