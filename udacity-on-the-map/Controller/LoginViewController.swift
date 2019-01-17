1c6ebda6ca03d486b4f84d07d579427f2199da3c
Shotaro Oyama <shotaro12oyama@gmail.com>
2019-01-18 06:12:54 +0900

    update@2019/1/18

--- udacity-on-the-map/Controller/LoginViewController.swift
+++ udacity-on-the-map/Controller/LoginViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019年 尾山昌太郎. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -34,7 +35,22 @@ class LoginViewController: UIViewController {
 
     @IBAction func loginTapped(_ sender: Any) {
         setLoggingIn(true)
-        UdacityClient.getRequestToken(completion: handleRequestTokenResponse(success:error:))
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        // encoding a JSON body from a string, can also use a Codable struct
+        request.httpBody = "{\"udacity\": {\"username\": \"shotaro.oyama@sumitomocorp.com\", \"password\": \"ooyy1954\"}}".data(using: .utf8)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let range = (5..<data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            print(String(data: newData!, encoding: .utf8)!)
+        }
+        task.resume()
     }
     /*
     // MARK: - Navigation
@@ -48,6 +64,7 @@ class LoginViewController: UIViewController {
     
     
 
+    
     
     @IBAction func loginViaWebsiteTapped() {
     }
--- udacity-on-the-map/Model/UdacityClient/UdacityClient.swift
+++ udacity-on-the-map/Model/UdacityClient/UdacityClient.swift
@@ -91,7 +91,7 @@ class UdacityClient {
                 }
             } catch {
                 do {
-                    let errorResponse = try decoder.decode(TMDBResponse.self, from: data) as Error
+                    let errorResponse = try decoder.decode(UdacityResponse.self, from: data) as Error
                     DispatchQueue.main.async {
                         completion(nil, errorResponse)
                     }
@@ -103,134 +103,9 @@ class UdacityClient {
             }
         }
         task.resume()
-        // encoding a JSON body from a string, can also use a Codable struct
-        request.httpBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".data(using: .utf8)
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { data, response, error in
-            if error != nil { // Handle error…
-                return
-            }
-            let range = Range(5..<data!.count)
-            let newData = data?.subdata(in: range) /* subset response data! */
-            print(String(data: newData!, encoding: .utf8)!)
-        }
-        task.resume()
+
     }
     
-    class func getWatchlist(completion: @escaping ([Movie], Error?) -> Void) {
-        taskForGETRequest(url: Endpoints.getWatchlist.url, responseType: MovieResults.self) { response, error in
-            if let response = response {
-                completion(response.results, nil)
-            } else {
-                completion([], error)
-            }
-        }
-    }
-    
-    class func getFavorites(completion: @escaping ([Movie], Error?) -> Void) {
-        taskForGETRequest(url: Endpoints.getFavorites.url, responseType: MovieResults.self) { response, error in
-            if let response = response {
-                completion(response.results, nil)
-            } else {
-                completion([], error)
-            }
-        }
-    }
-    
-    class func getRequestToken(completion: @escaping (Bool, Error?) -> Void) {
-        taskForGETRequest(url: Endpoints.getRequestToken.url, responseType: RequestTokenResponse.self) { response, error in
-            if let response = response {
-                Auth.requestToken = response.requestToken
-                completion(true, nil)
-            } else {
-                completion(false, error)
-            }
-        }
-    }
-    
-    class func login(username: String, password: String, completion: @escaping (Bool, Error?) -> Void) {
-        let body = LoginRequest(username: username, password: password, requestToken: Auth.requestToken)
-        taskForPOSTRequest(url: Endpoints.login.url, responseType: RequestTokenResponse.self, body: body) { response, error in
-            if let response = response {
-                Auth.requestToken = response.requestToken
-                completion(true, nil)
-            } else {
-                completion(false, error)
-            }
-        }
-    }
-    
-    class func createSessionId(completion: @escaping (Bool, Error?) -> Void) {
-        let body = PostSession(requestToken: Auth.requestToken)
-        taskForPOSTRequest(url: Endpoints.createSessionId.url, responseType: SessionResponse.self, body: body) { response, error in
-            if let response = response {
-                Auth.sessionId = response.sessionId
-                completion(true, nil)
-            } else {
-                completion(false, nil)
-            }
-        }
-    }
-    
-    /*
-    class func logout(completion: @escaping () -> Void) {
-        var request = URLRequest(url: Endpoints.logout.url)
-        request.httpMethod = "DELETE"
-        let body = LogoutRequest(sessionId: Auth.sessionId)
-        request.httpBody = try! JSONEncoder().encode(body)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            Auth.requestToken = ""
-            Auth.sessionId = ""
-            completion()
-        }
-        task.resume()
-    }
-    
-    class func search(query: String, completion: @escaping ([Movie], Error?) -> Void) -> URLSessionDataTask {
-        let task = taskForGETRequest(url: Endpoints.search(query).url, responseType: MovieResults.self) { response, error in
-            if let response = response {
-                completion(response.results, nil)
-            } else {
-                completion([], error)
-            }
-        }
-        return task
-    }
-    
-    class func markWatchlist(movieId: Int, watchlist: Bool, completion: @escaping (Bool, Error?) -> Void) {
-        let body = MarkWatchlist(mediaType: "movie", mediaId: movieId, watchlist: watchlist)
-        taskForPOSTRequest(url: Endpoints.markWatchlist.url, responseType: TMDBResponse.self, body: body) { response, error in
-            if let response = response {
-                // separate codes are used for posting, deleting, and updating a response
-                // all are considered "successful"
-                completion(response.statusCode == 1 || response.statusCode == 12 || response.statusCode == 13, nil)
-            } else {
-                completion(false, nil)
-            }
-        }
-    }
-    
-    class func markFavorite(movieId: Int, favorite: Bool, completion: @escaping (Bool, Error?) -> Void) {
-        let body = MarkFavorite(mediaType: "movie", mediaId: movieId, favorite: favorite)
-        taskForPOSTRequest(url: Endpoints.markFavorite.url, responseType: TMDBResponse.self, body: body) { response, error in
-            if let response = response {
-                completion(response.statusCode == 1 || response.statusCode == 12 || response.statusCode == 13, nil)
-            } else {
-                completion(false, nil)
-            }
-        }
-    }
-    
-    class func downloadPosterImage(path: String, completion: @escaping (Data?, Error?) -> Void) {
-        let task = URLSession.shared.dataTask(with: Endpoints.posterImage(path).url) { data, response, error in
-            DispatchQueue.main.async {
-                completion(data, error)
-            }
-        }
-        task.resume()
-    }
-    */
     
     
 }
