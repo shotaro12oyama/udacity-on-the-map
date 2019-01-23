d85a6e60760e16822a2367eeaf46f1f6c0a10953
Shotaro Oyama <shotaro12oyama@gmail.com>
2019-01-24 06:52:08 +0900

    Parse @2019/1/24

---
+++ udacity-on-the-map/Model/ParseClient/ParseClient.swift
@@ -0,0 +1,32 @@
+//
+//  ParseClient.swift
+//  udacity-on-the-map
+//
+//  Created by 尾山昌太郎 on 2019/01/24.
+//  Copyright © 2019年 尾山昌太郎. All rights reserved.
+//
+
+
+
+var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
+request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+let session = URLSession.shared
+let task = session.dataTask(with: request) { data, response, error in
+    if error != nil { // Handle error...
+        return
+    }
+    print(String(data: data!, encoding: .utf8)!)
+    
+    
+    let decoder = JSONDecoder()
+    do {
+        let parseResults = try decoder.decode(ParseResults.self, from: data!)
+        //let session = loginResponse.session
+    } catch {
+        print(error)
+    }
+    
+}
+task.resume()
+
---
+++ udacity-on-the-map/Model/ParseResponse/ParseResponse.swift
@@ -0,0 +1,29 @@
+//
+//  ParseClient.swift
+//  udacity-on-the-map
+//
+//  Created by 尾山昌太郎 on 2019/01/24.
+//  Copyright © 2019年 尾山昌太郎. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+struct ParseResponse : Codable{
+    let objectId: String
+    let uniqueKey: String?
+    let firstName: String
+    let lastName: String
+    let mapString: String
+    let mediaURL: String
+    let latitude: Float
+    let longitude: Float?
+    let createdAt: String
+    let updatedAt: String
+}
+
+struct ParseResults : Codable {
+    let results: [ParseResponse]
+}
